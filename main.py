8acadffb1e2982cf0d2662c8aab2ac6a49893928
Seolinu <seolinu92@daum.net>
2025-06-08 16:45:18 +0000

    Seolflix

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# GAS 웹 앱 URL (1-3에서 복사한 URL)
+GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyf9tUP89kTyteCfIuMB3rOjXekbO1QEIvholzs_G79bSFloXhUGlNTNJY90lqOnWoX/exec"
+
+@app.route('/')
+def index():
+    # GAS API에서 최신 메일 5개 요청
+    response = requests.get(GAS_WEB_APP_URL, params={'count': 1})
+    emails = []
+    if response.status_code == 200:
+        emails = response.json()
+
+    return render_template('index.html', emails=emails)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
